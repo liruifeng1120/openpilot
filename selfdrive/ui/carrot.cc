f914eb3b80c9b78afbccab58f3d3938ef212eab1
huheas <huheas@gmail.com>
2025-07-08 19:23:53 +0800

    适配PC 温度、内存、磁盘等数据读取

--- selfdrive/ui/carrot.cc
+++ selfdrive/ui/carrot.cc
@@ -2005,7 +2005,8 @@ public:
                 }
             }
         }
-	  }
+        makeDeviceInfo(s);
+	}
     void drawRadarInfo(UIState* s) {
         char str[128];
         int show_radar_info = params.getInt("ShowRadarInfo");
@@ -2492,17 +2493,18 @@ public:
         memoryUsage = deviceState.getMemoryUsagePercent();
         const auto cpuTempC = deviceState.getCpuTempC();
         const auto cpuUsagePercent = deviceState.getCpuUsagePercent();
-        int   size = sizeof(cpuTempC) / sizeof(cpuTempC[0]);
-        if (size > 0) {
-            for (int i = 0; i < size; i++) {
+        cpuTemp = 0.0f;
+        if (cpuTempC.size() > 0) {
+            for (int i = 0; i < cpuTempC.size(); i++) {
                 cpuTemp += cpuTempC[i];
             }
-            cpuTemp /= static_cast<float>(size);
+            cpuTemp /= static_cast<float>(cpuTempC.size());
         }
-        size = sizeof(cpuUsagePercent) / sizeof(cpuUsagePercent[0]);
-        if (size > 0) {
+
+        cpuUsage = 0.0f;
+        if (cpuUsagePercent.size() > 0) {
             int cpu_size = 0;
-            for (cpu_size = 0; cpu_size < size; cpu_size++) {
+            for (cpu_size = 0; cpu_size < cpuUsagePercent.size(); cpu_size++) {
                 if (cpuUsagePercent[cpu_size] <= 0) break;
                 cpuUsage += cpuUsagePercent[cpu_size];
             }
