723df0262e2c9fd96a28ee530e1b6dac396de7a0
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-09-27 19:07:58 +0800

    增加carrot_man

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -98,6 +98,8 @@ procs = [
   PythonProcess("otisserv", "selfdrive.navd.otisserv", always_run),
   PythonProcess("fleet_manager", "system.fleetmanager.fleet_manager", always_run, enabled=not PC),
 
+  PythonProcess("carrot_man", "selfdrive.fishsp.carrot_man", always_run),#, enabled=not PC),
+
   # debug procs
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
   PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
