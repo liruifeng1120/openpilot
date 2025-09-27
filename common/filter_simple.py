5ecf054c45519b66ed3f83fec0efd392e6da3eee
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-09-27 16:39:02 +0800

    增加滑动平均函数

--- common/filter_simple.py
+++ common/filter_simple.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import deque
 
 class FirstOrderFilter:
   # first order filter
@@ -43,3 +44,35 @@ class StreamingMovingAverage:
       self.sum -= self.values.pop(0)
     self.result = float(np.median(self.values)) if median else float(self.sum) / len(self.values)
     return self.result
+
+class MyMovingAverage:
+  def __init__(self, window_size, value=None):
+    self.window_size = window_size
+    if (value is not None):
+      self.values = deque([value] * window_size, maxlen=window_size)
+      self.sum = value * window_size
+      self.result = value
+    else:
+      self.values = deque(maxlen=window_size)
+      self.sum = 0
+      self.result = 0
+
+  def set(self, value):
+    self.values.clear()
+    self.values.append(value)
+    self.sum = value
+    self.result = value
+    return value
+
+  def set_all(self, value):
+    self.values = deque([value] * self.window_size, maxlen=self.window_size)
+    self.sum = value * self.window_size
+    self.result = value
+    return value
+
+  def process(self, value, median=False):
+    self.values.append(value)
+    self.sum = sum(self.values)
+    self.result = float(np.median(self.values)) if median else float(self.sum) / len(self.values)
+    return self.result
+
