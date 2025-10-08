3cf0bca11ba4b3291b3dd565ed8fed58d5829ca4
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-10-08 09:14:27 +0800

    同步pc-carrot2-v9: 支持PC环境下多接口检测广播地址

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -261,8 +261,21 @@ class CarrotMan:
     self.is_metric = self.params.get_bool("IsMetric")
 
   def get_broadcast_address(self):
+    # 修改为支持PC的多接口检测
     if PC:
-      iface = b'br0'
+        interfaces = ['wlan0', 'eth0', 'enp0s3', 'br0']  # 常见PC接口
+        for iface in interfaces:
+            try:
+                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                    ip = fcntl.ioctl(
+                        s.fileno(),
+                        0x8919,  # SIOCGIFBRDADDR
+                        struct.pack('256s', iface.encode('utf-8')[:15])
+                    )[20:24]
+                    return socket.inet_ntoa(ip)
+            except Exception:
+                continue
+        return "255.255.255.255"  # 回退地址
     else:
       iface = b'wlan0'
     try:
