29c252908e5299b45ef88c19a02c210dbca23a49
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-10-08 08:03:37 +0800

    build(modeld): 更新模型构建和运行配置 - 修改 SConscript 文件中的编译标志，简化 USB GPU 配置 - 更新 dmonitoringmodeld.py 和 modeld.py 中的环境变量设置 - 调整 TICI 和 USB GPU 的处理逻辑，增加 JIT 编译选项 refactor(modeld): 重构模型构建和运行脚本 - 更新 SConscript 文件，修改模型编译标志 - 优化 modeld.py 脚本，调整环境变量设置 - 适应新的硬件架构和编译要求

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 import os
 from openpilot.system.hardware import TICI
-os.environ['DEV'] = 'QCOM' if TICI else 'LLVM'
+#os.environ['DEV'] = 'QCOM' if TICI else 'LLVM'
 USBGPU = "USBGPU" in os.environ
 if USBGPU:
-  os.environ['DEV'] = 'AMD'
+  os.environ['GPU'] = '1'
   os.environ['AMD_IFACE'] = 'USB'
+elif TICI:
+  from openpilot.selfdrive.modeld.runners.tinygrad_helpers import qcom_tensor_from_opencl_address
+  os.environ['LLVM'] = '1'
+else:
+  os.environ['GPU'] = '1'
+  os.environ['JIT'] = '2'
 from tinygrad.tensor import Tensor
 from tinygrad.dtype import dtypes
 import time
