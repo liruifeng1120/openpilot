fd5bdbe341a8adbce887a77f6bd769ae3804ff6f
liruifeng1120 <2218808853@qq.com>
2025-12-05 02:05:21 +0800

    feat(locationd): torqued.py扩展车辆品牌支持列表
    新增对 Subaru、Volkswagen、Chrysler、Ford、GM、Mazda、Nissan 和 Tesla 品牌的支持，
    使系统能够兼容更多车型。

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -33,7 +33,7 @@ MIN_BUCKET_POINTS = np.array([100, 300, 500, 500, 500, 500, 300, 100])
 MIN_ENGAGE_BUFFER = 2  # secs
 
 VERSION = 1  # bump this to invalidate old parameter caches
-ALLOWED_CARS = ['toyota', 'hyundai', 'rivian', 'honda']
+ALLOWED_CARS = ['toyota', 'hyundai', 'rivian', 'honda', 'subaru', 'volkswagen', 'chrysler', 'ford', 'gm', 'mazda', 'nissan', 'tesla']  # 添加所有品牌支持
 
 
 def slope2rot(slope):
