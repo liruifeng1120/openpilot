50d88d13b601d4716cb9f1e4e92124ab55587f2a
openpilot <5604101+axi82@user.noreply.gitee.com>
2026-01-09 17:35:26 +0800

    dp的c3摄像头功能:强制参数dp_dev_dashy为2

--- dragonpilot/dashy/backend/server.py
+++ dragonpilot/dashy/backend/server.py
@@ -4,13 +4,13 @@ Copyright (c) 2025, Rick Lan
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, and/or sublicense, 
+to use, copy, modify, merge, publish, distribute, and/or sublicense,
 for non-commercial purposes only, subject to the following conditions:
 
-- The above copyright notice and this permission notice shall be included in 
+- The above copyright notice and this permission notice shall be included in
   all copies or substantial portions of the Software.
-- Commercial use (e.g. use in a product, service, or activity intended to 
-  generate revenue) is prohibited without explicit written permission from 
+- Commercial use (e.g. use in a product, service, or activity intended to
+  generate revenue) is prohibited without explicit written permission from
   the copyright holder.
 
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -118,8 +118,8 @@ async def init_api(request):
     try:
         params = Params()
         return web.json_response({
-            'is_metric': params.get_bool("IsMetric"), 
-            'dp_dev_dashy': int(params.get("dp_dev_dashy") or 0)
+            'is_metric': params.get_bool("IsMetric"),
+            'dp_dev_dashy': 2
         })
     except Exception as e:
         logging.getLogger("web_ui").error(f"Error fetching initial data: {e}")
