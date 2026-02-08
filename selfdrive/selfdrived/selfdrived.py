13700ab7464470faf0159743df3333b8eae53743
liruifeng1120 <2218808853@qq.com>
2026-02-08 19:17:05 +0800

    refactor(selfdrived): 移除驾驶员摄像头状态数据包
    移除了driverCameraState数据包，因为不再需要处理驾驶员摄像头数据，
    只保留roadCameraState和wideRoadCameraState用于道路视觉处理。
    ```

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -73,7 +73,7 @@ class SelfdriveD:
     self.gps_location_service = get_gps_location_service(self.params)
     self.gps_packets = [self.gps_location_service]
     self.sensor_packets = ["accelerometer", "gyroscope"]
-    self.camera_packets = ["roadCameraState", "driverCameraState", "wideRoadCameraState"]
+    self.camera_packets = ["roadCameraState", "wideRoadCameraState"]
 
     # TODO: de-couple selfdrived with card/conflate on carState without introducing controls mismatches
     self.car_state_sock = messaging.sub_sock('carState', timeout=20)
